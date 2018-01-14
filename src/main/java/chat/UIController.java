f9682c26985c1a3e3d97fd475729ade40c440708
Michael Munoz <mmunoz779@outlook.com>
2018-01-14 18:10:25 -0500

    Fixed centering of login VBox

--- src/main/java/chat/UIController.java
+++ src/main/java/chat/UIController.java
@@ -3,6 +3,7 @@ package chat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
@@ -31,10 +32,22 @@ public class UIController {
 
     @FXML
     public void handleLoginButtonClick() throws Exception {
+        loadAuthentication();
+    }
+
+    @FXML
+    public void handleLoginTextboxEnterPressed() throws Exception {
+        usernameField.setOnKeyPressed(e -> {
+            if (e.getCode().equals(KeyCode.ENTER))
+                loadAuthentication();
+        });
+    }
+
+    private void loadAuthentication() {
         chatTextArea.appendText("new username:\t" + usernameField.getText() + "\n");
         webView.setVisible(true);
         WebEngine webEngine = webView.getEngine();
-        webEngine.load("https://google.com");
+        webEngine.load("https://mixer.com");
         loginVBox.setVisible(false);
     }
 }
